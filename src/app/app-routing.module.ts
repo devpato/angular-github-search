cb4f8ea5c3962561ea681182f7f2ec5ec7e5a9f1
Patricio <wearetamo@gmail.com>
2019-05-20 14:10:36 -0400

    added basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { SearchPageComponent } from './components/search-page/search-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: 'search',
+    component: SearchPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
