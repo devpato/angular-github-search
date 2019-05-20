317e84cc329b159d9ba08e5617e6281059eda469
Patricio <wearetamo@gmail.com>
2019-05-20 14:58:49 -0400

    set up api user clal

--- src/app/shared/services/github-search.service.ts
+++ src/app/shared/services/github-search.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../modules/user.model';
 @Injectable({
   providedIn: 'root'
 })
 export class GithubSearchService {
+  BASE_URL = 'https://api.github.com/search/users'
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getUsers(user: string): Observable<User> {
+    let params = new HttpParams().set('q', user);
+    return this.http.get<User>(this.BASE_URL, { params: params });
+  }
 }
