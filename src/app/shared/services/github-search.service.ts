1fc2f3166dca3f29745b882dab9ae28fe6e11055
Patricio <wearetamo@gmail.com>
2019-05-21 10:57:51 -0400

    added repos api connection

--- src/app/shared/services/github-search.service.ts
+++ src/app/shared/services/github-search.service.ts
@@ -6,11 +6,17 @@ import { User } from '../modules/user.model';
   providedIn: 'root'
 })
 export class GithubSearchService {
-  BASE_URL = 'https://api.github.com/search/users'
+  BASE_URL = 'https://api.github.com';
+  USER_URL = '/search/users'
   constructor(private http: HttpClient) { }
 
   getUsers(user: string): Observable<User> {
     let params = new HttpParams().set('q', user);
-    return this.http.get<User>(this.BASE_URL, { params: params });
+    return this.http.get<User>(this.BASE_URL + this.USER_URL, { params: params });
+  }
+
+  getRepos(user: string): Observable<any> {
+    const REPOS_URL = '/users/' + user + '/repos';
+      return this.http.get<any>(this.BASE_URL + REPOS_URL);
   }
 }
