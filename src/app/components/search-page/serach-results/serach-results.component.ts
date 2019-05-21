5a7d78e769cb588e0cae31fd9d045e4bd854be01
Patricio <wearetamo@gmail.com>
2019-05-21 12:42:21 -0400

    get user details

--- src/app/components/search-page/serach-results/serach-results.component.ts
+++ src/app/components/search-page/serach-results/serach-results.component.ts
@@ -1,30 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { User } from 'src/app/shared/modules/user.model';
-import { GithubSearchService } from 'src/app/shared/services/github-search.service';
-import { Observable } from 'rxjs';
+import { Component, OnInit, Input } from "@angular/core";
+import { User } from "src/app/shared/modules/user.model";
+import { GithubSearchService } from "src/app/shared/services/github-search.service";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-serach-results',
-  templateUrl: './serach-results.component.html',
-  styleUrls: ['./serach-results.component.scss']
+  selector: "app-serach-results",
+  templateUrl: "./serach-results.component.html",
+  styleUrls: ["./serach-results.component.scss"]
 })
 export class SerachResultsComponent implements OnInit {
   @Input()
   users: User;
   reposCount = 0;
 
-
   $userRepos: Observable<any>;
-  constructor(private githubSearchService: GithubSearchService) { }
+  constructor(private githubSearchService: GithubSearchService) {}
 
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   getRepos(user: string) {
-    this.githubSearchService.getRepos(user).subscribe(repos=> {
-      console.log(repos.length);
+    this.githubSearchService.getRepos(user).subscribe(repos => {
       this.reposCount = repos.length;
     });
+  }
+
+  pullUsersDetails(user: string) {
+    this.getRepos(user);
   }
 }
