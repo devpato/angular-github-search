dfefebab7f38d02bfe1eb9e962f33bb1dcfdc3af
Patricio <wearetamo@gmail.com>
2019-05-21 12:42:34 -0400

    formatting

--- src/app/components/search-page/search-page.component.ts
+++ src/app/components/search-page/search-page.component.ts
@@ -1,19 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { GithubSearchService } from 'src/app/shared/services/github-search.service';
-import { Observable } from 'rxjs';
-import { User } from 'src/app/shared/modules/user.model';
+import { Component, OnInit } from "@angular/core";
+import { GithubSearchService } from "src/app/shared/services/github-search.service";
+import { Observable } from "rxjs";
+import { User } from "src/app/shared/modules/user.model";
 
 @Component({
-  selector: 'app-search-page',
-  templateUrl: './search-page.component.html',
-  styleUrls: ['./search-page.component.scss']
+  selector: "app-search-page",
+  templateUrl: "./search-page.component.html",
+  styleUrls: ["./search-page.component.scss"]
 })
 export class SearchPageComponent implements OnInit {
-  $users: Observable<User>
-  constructor(private githubSearchService: GithubSearchService) { }
+  $users: Observable<User>;
+  constructor(private githubSearchService: GithubSearchService) {}
 
   ngOnInit() {
-    this.$users = this.githubSearchService.getUsers('devpato');
+    this.$users = this.githubSearchService.getUsers("code");
   }
-
 }
