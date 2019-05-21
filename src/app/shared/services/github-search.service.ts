8d881b88b7cfa05ed9ce129ff08ed92706e24991
Patricio <wearetamo@gmail.com>
2019-05-21 12:42:54 -0400

    get repos ready

--- src/app/shared/services/github-search.service.ts
+++ src/app/shared/services/github-search.service.ts
@@ -1,22 +1,25 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { User } from '../modules/user.model';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { User } from "../modules/user.model";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class GithubSearchService {
-  BASE_URL = 'https://api.github.com';
-  USER_URL = '/search/users'
-  constructor(private http: HttpClient) { }
+  BASE_URL = "https://api.github.com";
+  USER_URL = "/search/users";
+  constructor(private http: HttpClient) {}
 
   getUsers(user: string): Observable<User> {
-    let params = new HttpParams().set('q', user);
-    return this.http.get<User>(this.BASE_URL + this.USER_URL, { params: params });
+    let params = new HttpParams().set("q", user);
+    return this.http.get<User>(this.BASE_URL + this.USER_URL, {
+      params: params
+    });
   }
 
   getRepos(user: string): Observable<any> {
-    const REPOS_URL = '/users/' + user + '/repos';
-      return this.http.get<any>(this.BASE_URL + REPOS_URL);
+    console.log(user);
+    const REPOS_URL = "/users/" + user + "/repos";
+    return this.http.get<any>(this.BASE_URL + REPOS_URL);
   }
 }
