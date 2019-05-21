127770c72cd99d17083d7b97277f1c331b7e5b80
Patricio <wearetamo@gmail.com>
2019-05-21 16:09:39 -0400

    angular reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from "./app-routing.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ReactiveFormsModule } from "@angular/forms";
 
 /* Shared Components */
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
-import { SearchComponent } from './shared/components/search/search.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
-import { AppComponent } from './app.component';
+import { NavbarComponent } from "./shared/components/navbar/navbar.component";
+import { SearchComponent } from "./shared/components/search/search.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
+import { AppComponent } from "./app.component";
 
 /*Home Page components */
-import { HomePageComponent } from './components/home-page/home-page.component';
+import { HomePageComponent } from "./components/home-page/home-page.component";
 
 /*Search Page components */
-import { SearchPageComponent } from './components/search-page/search-page.component';
-import { SerachResultsComponent } from './components/search-page/serach-results/serach-results.component';
-import { SearchHeaderComponent } from './components/search-page/search-header/search-header.component';
+import { SearchPageComponent } from "./components/search-page/search-page.component";
+import { SerachResultsComponent } from "./components/search-page/serach-results/serach-results.component";
+import { SearchHeaderComponent } from "./components/search-page/search-header/search-header.component";
 
 /*PrimeNG */
-import {DataViewModule} from 'primeng/dataview';
-import {PanelModule} from 'primeng/panel';
+import { DataViewModule } from "primeng/dataview";
+import { PanelModule } from "primeng/panel";
 
 @NgModule({
   declarations: [
@@ -39,9 +40,10 @@ import {PanelModule} from 'primeng/panel';
     HttpClientModule,
     DataViewModule,
     PanelModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { GithubSearchService } from '../../services/github-search.service';
 import { Observable } from 'rxjs';
 import { User } from '../../modules/user.model';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -10,10 +12,21 @@ import { User } from '../../modules/user.model';
 })
 export class SearchComponent implements OnInit {
   $githubSearch: Observable<User>;
+  searchForm = this.fb.group({
+    searchParam: ['', Validators.required]
+  });
 
-  constructor(private githubSearchService: GithubSearchService) { }
+  constructor(
+    private githubSearchService: GithubSearchService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit() {
     this.$githubSearch = this.githubSearchService.getUsers('devpato');
+  }
+
+  onSearch(): void {
+    this.githubSearchService.setSearchParam(this.searchForm.value.searchParam);
+    this.searchForm.reset();
   }
 }
